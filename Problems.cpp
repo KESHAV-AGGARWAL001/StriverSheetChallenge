0d626baa9b2df5aeea3092dd4b239549af991cb5
KESHAV MITTAL <102140152+KESHAV-AGGARWAL001@users.noreply.github.com>
2023-06-17 17:32:28 +0530

    Update Problems.cpp

--- Problems.cpp
+++ Problems.cpp
@@ -15,3 +15,50 @@ int subarraysXor(vector<int> &arr, int x)
     }
     return count;
 }
+
+
+//  maximum of every window size 
+#include <bits/stdc++.h> 
+vector<int> maxMinWindow(vector<int> a, int n) {
+   
+   stack<int> s;
+   vector<int> prev_min(n+1);   
+   vector<int> next_min(n+1);
+
+   for(int i=0; i<n;i++) prev_min[i] = -1 , next_min[i] = n;
+    
+    for(int i=0; i<n;i++){
+        while(!s.empty() and a[s.top()] >= a[i]){
+            s.pop();
+        }
+        if(!s.empty()) prev_min[i] = s.top();
+        s.push(i);
+    }
+
+    while(s.size()) s.pop();
+    
+    for(int i=n-1;i>=0;i--){
+         while(!s.empty() and a[s.top()] >= a[i]){
+            s.pop();
+        }
+        if(!s.empty()) next_min[i] = s.top();
+        s.push(i);
+    }
+
+    while(s.size()) s.pop();
+    
+    vector<int> result (n+1, INT_MIN);
+
+    for(int i=0; i<n;i++){
+        int length = next_min[i] - prev_min[i] - 1;
+        
+        result[length] = max(result[length] , a[i]);
+    }
+
+    for(int i=n-1;i>=1;i--){
+        result[i] = max(result[i], result[i+1]);
+    }
+    
+    result.erase(result.begin());
+    return result;
+}

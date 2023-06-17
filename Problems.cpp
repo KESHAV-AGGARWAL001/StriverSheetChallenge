7424a2fe34bdac40a94de1446bfb2e10156278e6
KESHAV MITTAL <102140152+KESHAV-AGGARWAL001@users.noreply.github.com>
2023-06-18 00:17:23 +0530

    Update Problems.cpp

--- Problems.cpp
+++ Problems.cpp
@@ -188,3 +188,34 @@ vector<int> pathInATree(TreeNode<int> *root, int x)
 	dfs(root, ans , x );
 	return {};
 }
+
+
+//  maximum product subarray 
+//  I used idea of kadane algorithm 
+
+#include <bits/stdc++.h> 
+int maximumProduct(vector<int> &nums, int n)
+{
+	int maxi = INT_MIN;
+	int prod=1;
+
+	for(int i=0;i<n;i++)
+	{
+		prod*=nums[i];
+		maxi=max(prod,maxi);
+		if(prod==0)
+		prod=1;
+	}
+	prod=1;
+	for(int i=n-1;i>=0;i--)
+	{
+		prod*=nums[i];
+
+		maxi=max(prod,maxi);
+		if(prod==0)
+		prod=1;
+	}
+	return maxi;
+}
+
+

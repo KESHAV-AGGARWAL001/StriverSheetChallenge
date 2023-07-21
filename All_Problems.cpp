555586192de330a855e5da75d929e8894cca46a8
KESHAV MITTAL <102140152+KESHAV-AGGARWAL001@users.noreply.github.com>
2023-07-21 16:09:36 +0530

    Update All_Problems.cpp

--- All_Problems.cpp
+++ All_Problems.cpp
@@ -4304,3 +4304,17 @@ int editDistance(string a, string b)
     return dp[n][m];
 }
 
+// minimum number of coins 
+vector<int> MinimumCoins(int amount)
+{
+    vector<int> result;
+    vector<int> coins = {1000, 500 , 100 , 50 , 20 , 10 , 5, 2 ,1};
+    for(auto it : coins){
+        if(amount>=it){
+            for(int i=0; i<amount/it;i++)
+                result.emplace_back(it);
+            amount %= it;
+        }
+    }
+    return result;
+}

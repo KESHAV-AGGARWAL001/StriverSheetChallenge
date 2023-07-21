997e286e1a0a67ceccd6146c3dca075d4318268c
KESHAV MITTAL <102140152+KESHAV-AGGARWAL001@users.noreply.github.com>
2023-07-21 15:48:02 +0530

    Update All_Problems.cpp

--- All_Problems.cpp
+++ All_Problems.cpp
@@ -4277,3 +4277,30 @@ int lcs(string text1, string text2)
 	return max(0,dp[n][m]);
 }
 
+//  edit distance 
+
+#include "bits/stdc++.h"
+
+int editDistance(string a, string b)
+{
+    int n=a.length() ;
+    int m = b.length();
+
+    vector<vector<int>> dp( n+1 , vector<int>(m+1 , 0));
+
+    for(int i=1; i<= n;i++) dp[i][0] = i;
+    for(int j=1; j<=m; j++) dp[0][j] = j;
+
+    for(int i=1; i<=n;i++){
+        curr[i] = i;
+        for(int j=1; j<=m;j++){
+            if(a[i-1]==b[j-1]) dp[i][j] = dp[i-1][j-1];
+            else{
+                dp[i][j] = 1+ min({dp[i-1][j] , dp[i][j-1] , dp[i-1][j-1]});
+            }
+        }
+
+    }
+    return dp[n][m];
+}
+

7edfae070bd1f3755b801287f86284b9ea22e6c7
KESHAV MITTAL <102140152+KESHAV-AGGARWAL001@users.noreply.github.com>
2023-07-21 15:01:38 +0530

    Update All_Problems.cpp

--- All_Problems.cpp
+++ All_Problems.cpp
@@ -4260,5 +4260,20 @@ BinaryTreeNode<int>* BTtoDLL(BinaryTreeNode<int>* root) {
     return result;
 }
 
+//  longest common subsequence 
 
+#include<bits/stdc++.h>
+
+int lcs(string text1, string text2)
+{
+	int n = text1.length() ;
+	int m = text2.length();
+	vector<vector<int>> dp(n+1 , vector<int> (m+1,0));
+	for(int i=1; i<=n;i++){
+		for(int j=1 ;j<=m;j++){
+			dp[i][j] = (text1[i-1] == text2[j-1]) ? 1 + dp[i-1][j-1] : max(dp[i-1][j] , dp[i][j-1]);
+		}
+	}
+	return max(0,dp[n][m]);
+}
 
